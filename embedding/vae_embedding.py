1be78b6b9f051bec31ed79ac2db9fe123248dcda
dongminleeai <kid33629@gmail.com>
2020-04-21 12:58:46 +0900

    Commit

--- embedding/vae_embedding.py
+++ embedding/vae_embedding.py
@@ -59,7 +59,7 @@ def main():
     dir_name = 'runs/' + args.env + '/' \
                                   + 'ds_' + str(np.array(dataset).shape[0]) \
                                   + '_ep_' + str(args.epochs) \
-                                  + '_1e-2_t_' + datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
+                                  + '_1e-3_t_' + datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
     writer = SummaryWriter(log_dir=dir_name)
 
     start_time = time.time()
@@ -86,7 +86,7 @@ def main():
             kld = - 0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp()).to(device)
 
             # Update VAE model parameters
-            loss = reconst + 1e-2 * kld   # default: 1e-4
+            loss = reconst + 1e-3 * kld   # default: 1e-4
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
@@ -131,7 +131,7 @@ def main():
                                          + '_ep_' + str(args.epochs) \
                                          + '_al_' + str(round(average_loss, 2)) \
                                          + '_el_' + str(round(loss.item(), 2)) \
-                                         + '_1e-2_t_' + str(int(time.time() - start_time)) 
+                                         + '_1e-3_t_' + str(int(time.time() - start_time)) 
                                          + '.pt')
     
     torch.save(model.state_dict(), ckpt_path)
