556f99dd1b24c394f2eed7017211710f9ebeadbf
dongminleeai <kid33629@gmail.com>
2020-04-26 12:56:01 +0900

    Commit

--- rl/run_hopper.py
+++ rl/run_hopper.py
@@ -44,7 +44,7 @@ def main():
     # Create an agent                                                   
     agent = Agent(env, args, device, obs_dim, act_dim, act_limit, 
                   hidden_sizes=(300,300), buffer_size=int(1e6), batch_size=100, 
-                  alpha=0.2, actor_lr=1e-4, qf_lr=1e-4)   
+                  alpha=0.2, actor_lr=1e-3, qf_lr=1e-3)   
 
     # Create a SummaryWriter object by TensorBoard
     dir_name = 'runs/' + 'Hopper/' + args.mode + '_hs_300_alr_1e-4_clr_1e-4'
